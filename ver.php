4aa0608f9cf6b2520f3ae40dd65acc15c0ed7063
rolandoute <122328200+rolandoute@users.noreply.github.com>
2023-01-10 20:41:11 -0500

    Create ver.php

---
+++ ver.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Almacenar imagen en la base de datos MySQL usando PHP</title>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<style type="text/css">
+*{ font-family:Segoe, "Segoe UI", "DejaVu Sans", "Trebuchet MS", Verdana, sans-serif}
+.main{ margin:auto; border:1px solid #7C7A7A; width:60%; text-align:left; padding:30px; background:#85c587}
+input[type=submit]{ background:#6ca16e; width:100%;
+    padding:5px 15px; 
+    background:#ccc; 
+    cursor:pointer;
+	font-size:16px;
+   
+}
+input[type=text]{  width:40%;
+    padding:5px 15px; height:25px;
+	font-size:16px;
+   
+}
+.form-control {
+	padding: 0px 0px;
+}
+</style>
+</head>
+<body bgcolor="#bed7c0"><br>
+<div class="main">
+<h1>Mostrando imagen almacenada en MySQL</h1>
+  <div class="panel panel-primary">
+    <div class="panel-body">
+<img src='vista.php?id=4' alt='Img blob desde MySQL' width="600" />      
+ 	  </div> 
+    </div>
+ </div>
+</body>
+</html>
